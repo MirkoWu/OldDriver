9ef83f837a9b6bc24c1968537206ff6825067359
MirkoWu <709651717@qq.com>
2017-11-22 11:49:28 +0800

    add anim

--- app/src/main/java/com/mirkowu/olddriver/ui/splash/SplashActivity.java
+++ app/src/main/java/com/mirkowu/olddriver/ui/splash/SplashActivity.java
@@ -1,10 +1,12 @@
 package com.mirkowu.olddriver.ui.splash;
 
 
-import android.os.Looper;
+import android.animation.Animator;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.mirkowu.olddriver.R;
 import com.mirkowu.olddriver.databinding.ActivitySplashBinding;
+import com.mirkowu.olddriver.ui.MainActivity;
 import com.softgarden.baselibrary.base.databinding.DataBindingActivity;
 import com.softgarden.baselibrary.widget.CommonToolbar;
 
@@ -22,6 +24,30 @@ public class SplashActivity extends DataBindingActivity<ActivitySplashBinding> {
 
     @Override
     protected void initialize() {
+        LottieAnimationView animationView = findViewById(R.id.mLottieAnimationView);
+        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                openActivity(MainActivity.class);
+                finish();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        animationView.setSpeed(2f);
     }
 
     @Override
